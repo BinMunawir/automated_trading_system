856ca8a7327f0f07f9c3868601617e5400554ab8
abm <bu3abed.mun@gmail.com>
2020-05-13 20:57:43 +0300

    impl: signal class

---
+++ rms/src/rms/Signal.java
@@ -0,0 +1,36 @@
+package rms;
+
+public class Signal {
+    String symbol;
+    String type;
+    String strategy;
+    int probability;
+    boolean isRiskVerified;
+    double entryPrice;
+    double takeProfit;
+    double stopLoss;
+
+    public Signal(String symbol, String type, String strategy, int probability, double entryPrice, double takeProfit, double stopLoss) {
+        this.symbol = symbol;
+        this.type = type;
+        this.strategy = strategy;
+        this.probability = probability;
+        this.entryPrice = entryPrice;
+        this.takeProfit = takeProfit;
+        this.stopLoss = stopLoss;
+    }
+
+    public void verifyRisk() {
+        if (this.takeProfit/this.stopLoss >= Risk.r) {
+            this.isRiskVerified = false;
+            return;
+        }
+        double singleLoss = this.entryPrice - this.stopLoss;
+        double totalAcceptedLoss = Risk.risk/100 * Risk.capital;
+        if (singleLoss > totalAcceptedLoss) {
+            this.isRiskVerified = false;
+            return;
+        }
+        this.isRiskVerified = true;
+    }
+}
